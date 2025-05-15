ddceb6f2b8d9ace5cdb5fe9a5a896fd76f99e6ff
Nickpt98 <nikhil.patil@serverlane.tech>
2025-05-15 18:37:01 +0530

    removed user from seed

--- data/seeds/1.users.js
+++ data/seeds/1.users.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 exports.seed = async (knex) => {
+  return;
   return knex("users")
     .del()
     .then(async () => {
@@ -8,7 +9,7 @@ exports.seed = async (knex) => {
       return knex("users").insert([
         {
           user_id: uuidv4(),
-          email: "admin1@gmail.com",
+          email: "admin@gmail.com",
           pacdora_user_id: "x9YP6Q7iCzEkhy2n4XfgbHLsmowqJAZ",
           password: await bcrypt.hash("Abcd@1234", salt),
           is_email_verified: true,
